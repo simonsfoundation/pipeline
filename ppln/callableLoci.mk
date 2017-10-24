ad7a9018e2a51878f889848ab779f3cae44787b4
Irina Astrovskaya <iastrovskaya@github.com>
2017-10-24 14:53:30 -0400

    minor updates

--- ppln/callableLoci.mk
+++ ppln/callableLoci.mk
@@ -14,6 +14,7 @@ LOGDIR = $(OUTDIR)
 TMPDIR = /tmp/$(USR)/$(PROJ)
 ###
 
+
 inBam = $(wildcard $(INDIR)/$(PREFIX)*$(SUFFIX).bam)
 $(info $(inBam))
 #inBed = $(patsubst %$(SUFFIX).bam,%$(SUFFIX).bed,$(inBam))
